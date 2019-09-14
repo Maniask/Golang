b54c3ccbc26313962276a2d7693c657fc99fd5f8
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-09-15 02:52:55 +0530

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,13 @@
+/* 
+  This is the program for printing "Hello, World".
+  Created By: Mani Agarwal
+  Email : agarwalmani22@gmail.com
+*/
+
+package main 
+
+import "fmt" // fmt is the package for "print".
+
+func main() {   
+    fmt.Println("Hello, World") 
+}
