aa2bc85b80e9946f0f43f9813cf42f76d4dfd741
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2020-12-19 19:06:45 +0530

    Create type&size.go

---
+++ type&size.go
@@ -0,0 +1,19 @@
+/* 
+  This is the program for find the type and size of the number.
+  Created By: Mani Agarwal
+  Email : agarwalmani22@gmail.com
+*/
+
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	a, b := 89,95
+	fmt.Println("Value of a is ",a , "& b is ", b)
+	fmt.Printf("Type of a is %T, Size of a is %d ", a, unsafe.Sizeof(a)) 
+	fmt.Printf("\nType of b is %T, Size of b is %d ", b, unsafe.Sizeof(b)) 
+}
